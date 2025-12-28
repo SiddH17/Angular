2ff0ff223e4be46a7c0663276cb18b5c647f777d
SiddH17 <77070648+SiddH17@users.noreply.github.com>
2025-12-29 01:16:38 +0530

    Delete testproject directory

--- testproject/src/app/courses.service.ts
+++
@@ -1,8 +0,0 @@
-
-//Always use the name of the class used
-//Service is normally a plain class, so decorators are not used
-export class CoursesService {
-    getCourses()    {
-        return ['author1','author2','author3'];    
-    }
-}
